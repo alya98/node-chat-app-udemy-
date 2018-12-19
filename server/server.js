8d9ad1e6b2328d6766bed4d1fdd45a9a139a8b9d
mac-185-71 <mac-185-71@mac-185-71icxlocal.local>
2018-12-19 16:23:42 +0300

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -15,6 +15,9 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('new user connected')
 
+  socket.emit('newMessage', {from: 'admin', text: 'welcome to the chat app',  createdAt: new Date().getTime()})
+  socket.broadcast.emit('newMessage', {from: 'admin', text: 'new user joined',  createdAt: new Date().getTime()})
+
   socket.on('createMessage', (newMessage) => {
     console.log('createMessage', newMessage)
     io.emit('newMessage', {
