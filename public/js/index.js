998caa0facd69b0fb05dede897ac41f74844177a
mac-185-71 <mac-185-71@mac-185-71icxlocal.local>
2018-12-19 16:03:07 +0300

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ const socket = io();
 
 socket.on('connect', function() {
   console.log('connected to server')
-
-  socket.emit('createMessage', {
-    from: 'artem',
-    text: 'Hey, this is alina'
-  });
 });
 
 socket.on('disconnect', function() {
--- server/server.js
+++ server/server.js
@@ -15,14 +15,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('new user connected')
 
-  socket.emit('newMessage', {
-    from: 'alina',
-    text: 'hello',
-    createdAt: 123
-  })
-
   socket.on('createMessage', (newMessage) => {
     console.log('createMessage', newMessage)
+    io.emit('newMessage', {
+      from: newMessage.from,
+      text: newMessage.text,
+      createdAt: new Date().getTime()
+    })
   })
 
   socket.on('disconnect', () => {
